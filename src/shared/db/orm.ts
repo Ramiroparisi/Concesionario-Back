9e8b9b773f25f12003123733fd26596e6ddb2d94
Ramiro <ramiroparisi678@gmail.com>
2026-05-05 14:11:21 -0300

    Arreglo error de importacion de ts

--- src/shared/db/orm.ts
+++ src/shared/db/orm.ts
@@ -2,19 +2,21 @@ import { defineConfig } from '@mikro-orm/mysql';
 import { SqlHighlighter } from '@mikro-orm/sql-highlighter';
 import { TsMorphMetadataProvider } from '@mikro-orm/reflection';
 
-import { Marca } from '../../marca/marca.entity.js';
+/*import { Marca } from '../../marca/marca.entity.js';
 import { Modelo } from '../../modelo/modelo.entity.js';
 import { Multimedia } from '../../multimedia/multimedia.entity.js';
 import { Reserva } from '../../reserva/reserva.entity.js';
 import { Usuario } from '../../usuario/usuario.entity.js';
 import { Vehiculo } from '../../vehiculo/vehiculo.entity.js';
-import {BaseEntity} from '../db/baseEntity.entity.js';
+import {BaseEntity} from '../db/baseEntity.entity.js';*/
 import {config} from 'dotenv';
 
 config();
 
 export default defineConfig({
-  entities: [Marca, Modelo, Vehiculo, Usuario, Reserva, Multimedia, BaseEntity],
+  entities: ['./dist/**/*.entity.js'],
+  entitiesTs: ['./src/**/*.entity.ts'],
+
   clientUrl: process.env.BDD_URL, 
   metadataProvider: TsMorphMetadataProvider,
   highlighter: new SqlHighlighter(),
